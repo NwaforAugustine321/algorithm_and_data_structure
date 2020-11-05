8477c90140114545c14fa4b4c4ec835f6f1468c7
thaddydore <thaddydore@gmail.com>
2020-11-05 12:54:08 +0100

    wip: pop method

--- singleLinkedList/singleLinkedList.js
+++ singleLinkedList/singleLinkedList.js
@@ -1,8 +1,8 @@
-// a node is the smallest unit of a tree which contains a data[this can be in any [type]
-// it also contains a link to next node otherwise known as edges
+// a node is the smallest unit of a tree which contains a data[this can be any [type].
+// it also contains a link to the next node otherwise known as edges.
 
-// linked list is an array like structure that doesn't that is  not indexed. 
-// it has a head, tail and length attributes
+// linked list is an array like structure that doesn't have index. 
+// it has a head, tail and length attributes.
 
 class Node {
   constructor(val) {
@@ -11,10 +11,51 @@ class Node {
   }
 }
 
-class singleLinkedList {
+class SingleLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
+
+const singleLinkedList = new SingleLinkedList();
+singleLinkedList.push('first')
+singleLinkedList.push('second')
+singleLinkedList.push('third')
+singleLinkedList.push('fourth')
+singleLinkedList.pop('fourth')
+singleLinkedList.pop('fourth')
+console.log(singleLinkedList);
