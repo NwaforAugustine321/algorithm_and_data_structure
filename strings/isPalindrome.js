ceede6464be479d9a5b64c59c41531a1e9c8d56b
NwaforAugustine321 <nwaforaugustine422@gmail.com>
2022-11-23 12:53:00 +0100

    solved ispalindrome question

---
+++ strings/isPalindrome.js
@@ -0,0 +1,38 @@
+//A phrase is a palindrome if, after converting all uppercase letters into lowercase
+// letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+//Alphanumeric characters include letters and numbers.
+
+function isPalindrome(s) {
+  return validateString(resetString(s.split('')));
+}
+
+function resetString(s) {
+  let _validString = '';
+  for (let i = 0; i < s.length; i++) {
+    if (/[a-z+0-9]/.test(s[i]) || /[A-Z+0-9]/.test(s[i])) {
+      _validString = _validString + s[i].toLowerCase();
+    }
+  }
+
+  return _validString;
+}
+
+function validateString(s) {
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] != s[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+let input = 'A man, a plan, a canal: Panama';
+
+const result = isPalindrome(input);
+console.log(result);
